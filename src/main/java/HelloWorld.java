5ecc561ffe2c2f9ba1a1de7a3a0a62ff79bc3c1c
Ryan Zhao <6555985@student.swin.edu.au>
2012-08-22 17:11:43 +1000

    test 2

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        resp.getWriter().print("HELLO HIT3311 Software Deployment and Evolution!\n");
+        resp.getWriter().print("123 HELLO HIT3311 Software Deployment and Evolution!\n");
     }
 
     public static void main(String[] args) throws Exception{
