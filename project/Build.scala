afd239c71bf58421888582b13a327f735bb189c6
Ryan Zhao <6555985@student.swin.edu.au>
2012-08-25 15:49:50 +1000

    change project name to HIT3311,not helloworld

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "helloworld"
+    val appName         = "HIT3311"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
